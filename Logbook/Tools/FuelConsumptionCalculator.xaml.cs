b4a8d538146cbe2b82621000d2fb988f6a3d422e
bjakoktochce <grypsy@gmail.com>
2020-07-11 13:05:01 +0200

    Fuel consumption Calculator major changes - added some fields and initial calculation function

--- Logbook/Tools/FuelConsumptionCalculator.xaml.cs
+++ Logbook/Tools/FuelConsumptionCalculator.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Logbook.Tools
 {
@@ -24,9 +25,49 @@ namespace Logbook.Tools
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+        private void TextBoxConsumptionPerHour_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+        private void TextBoxTimeOfFlight_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private void ButtonCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxConsumptionPerHour.Text.Length == 0)
+            {
+                TextBoxConsumptionPerHour.BorderBrush = Brushes.Red;
+            }
+            //else TextBoxTimeOfFlight.BorderBrush = Brushes.Black;
+
+            if (TextBoxTimeOfFlight.Text.Length == 0)
+            {
+                TextBoxTimeOfFlight.BorderBrush = Brushes.Red;
+            }
+
+            //else TextBoxTimeOfFlight.BorderBrush = Brushes.Black;
+
+            if ((TextBoxConsumptionPerHour.Text.Length != 0) && (TextBoxTimeOfFlight.Text.Length != 0))
+            {
+                Double fuelNeeded = 0;
+                fuelNeeded = Convert.ToDouble(TextBoxConsumptionPerHour.Text) * Convert.ToDouble(TextBoxTimeOfFlight.Text) / 60;
+
+                TextBoxFuelNeeded.Text = Convert.ToString(fuelNeeded);
+
+            }
+           
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
-        }
+        }       
     }
 }
